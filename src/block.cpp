eec2e3bbbaea11682bd2f93e00849a021aba80a0
JitChowDhury <jitchowdhury8717@gmail.com>
2025-10-12 23:07:29 +0530

    Comment

--- src/block.cpp
+++ src/block.cpp
@@ -9,9 +9,11 @@ Block::Block()
 
 void Block::Draw()
 {
+	// Get current rotation shape
 	std::vector<Position> tiles = cells[rotationState];
 	for (Position item : tiles)
 	{
+		// Draw each tile at its grid position using the block’s color
 		DrawRectangle(item.column * cellSize + 1, item.row * cellSize + 1, cellSize - 1, cellSize - 1, colors[id]);
 	}
 }
